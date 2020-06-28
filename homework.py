fef07b3219bff832effd0d2981fa257fc9b8f6ef
Ольга <oe.goncharova@gmail.com>
2020-06-28 16:26:53 +0300

    my second try api_01_sms

--- homework.py
+++ homework.py
@@ -6,7 +6,13 @@ from twilio.rest import Client
 
 access_token = os.getenv('VK_token')
 version = 5.92
-url = 'https://api.vk.com/method/'
+url = 'https://api.vk.com/'
+constants = {
+    'account_sid': os.getenv('account_sid'),
+    'auth_token': os.getenv('auth_token')
+}
+from_ = os.getenv('NUMBER_FROM')
+to = os.getenv('NUMBER_TO')
 
 
 def get_status(user_id):
@@ -17,32 +23,29 @@ def get_status(user_id):
         'fields': 'online',
     }
     user_status = requests.post(
-        f'{url}users.get', params=params)
+        f'{url}method/users.get', params=params)
     return user_status.json()['response'][0]['online']
 
 
-class SendSms:
-    def __init__(self):
-        self.account_sid = os.getenv('account_sid')
-        self.auth_token = os.getenv('auth_token')
-        self.client = Client(self.account_sid, self.auth_token)
-        self.body = self.sms_text,
-        self.from_ = os.getenv('NUMBER_FROM')
-        self.to = os.getenv('NUMBER_TO')
+def call_client():
+    client = Client(constants)
+    return(client)
 
-    def sms_sender(self, sms_text):
-        self.message = self.client.messages.create(
-            body=self.body,
-            from_=self.from_,
-            to=self.to
-        )
-        return(self.message.sid)
+
+def sms_sender(sms_text):
+    client = call_client()
+    message = client.messages.create(
+        body=sms_text,
+        from_=from_,
+        to=to
+    )
+    return(message.sid)
 
 
 if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
         if get_status(vk_id) == 1:
-            SendSms.sms_sender(f'{vk_id} сейчас онлайн!')
+            sms_sender(f'{vk_id} сейчас онлайн!')
             break
         time.sleep(5)
